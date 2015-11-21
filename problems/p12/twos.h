a703c4840a5a088c73dc80cda9581423423a94ca
tkim057 <tkim057@ucr.edu>
2015-11-21 10:57:51 +0000

    Kyle is a sucky meanie

--- problems/p12/twos.h
+++ problems/p12/twos.h
@@ -1,6 +1,21 @@
-static int twos(int n)
-{
-	(void)n;
-	/*code goes here */
+static int twos(int n){
+	    int n = 250;
+        int count=0;
+        int counter = 0;
+        for(int i = 1; i <= n; i++){
+            if((i % 10)==2){
+               count++;
+            }
+            if(i-(i%10)==20){
+               count++;
+            }
+            if(i/10-(i%10)==20){
+               count++;
+            }
+            if(i/10-(i%10)==20){
+               count++;
+            }
+       }
+       cout << count << endl;
 	return 0;
 }
