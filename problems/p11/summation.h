7a06efc7f0690588cb5bb279790858254de289e0
tkim057 <tkim057@ucr.edu>
2015-11-21 12:50:12 +0000

    KYLE COMPLIE JDFNJBHFBHBFJBHRFBMDNFBHFHIBGDFHNBFHJKFB

--- problems/p11/summation.h
+++ problems/p11/summation.h
@@ -1,10 +1,13 @@
 #include <vector>
 static std::vector<std::pair<int, int> > summation(std::vector<int> &v, int n)
 {
-	/* code goes here */
-	(void)n;
-	(void)v;
-	std::pair<int, int> p(0, 0);
-	std::vector<std::pair<int, int> > ans {p};
-	return ans;
+   std::vector<std::pair<int, int> > name;
+   for(int i = 1; i <= v.size(); i++){
+       for(int j = i; j <= v.size(); j++){
+           if (i+j==n){
+               name.push_back(std::pair(i,j));
+           }
+       }
+   }
+   return name;
 }
