1e7ec5e36d5b3da0c73d926e56ccf5ab6d18e138
tkim057 <tkim057@ucr.edu>
2015-11-21 12:07:25 +0000

    kyle sucks

--- problems/p12/twos.h
+++ problems/p12/twos.h
@@ -1,8 +1,8 @@
 static int twos(int n){
     int count=0;
     for(int i = 1; i <= n; i++){
-        for(int j = 0; j < (std::to_string(i)).length(); j++ ){
-        	if ((std::to_string(i)).at(j) == '2'){
+        for(int j = 0; j < (std::string to_string(i)).length(); j++ ){
+        	if ((std::string to_string(i)).at(j) == '2'){
         		count++;
         	}
         }
