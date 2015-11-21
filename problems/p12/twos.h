89222ad2b35ae0a92834fd2de199ffa574164cac
tkim057 <tkim057@ucr.edu>
2015-11-21 12:14:56 +0000

    PLees work

--- problems/p12/twos.h
+++ problems/p12/twos.h
@@ -2,7 +2,7 @@
 static int twos(int n){
     int count=0;
     for(int i = 1; i <= n; i++){
-        for(int j = 0; j < (std::to_string(i)).length(); j++ ){
+        for(unsigned j = 0; j < (std::to_string(i)).length(); j++ ){
         	if ((std::to_string(i)).at(j) == '2'){
         		count++;
         	}
