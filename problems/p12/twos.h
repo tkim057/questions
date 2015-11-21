0358ccba9feb7fa98028a5a4649af3b41f06ef70
tkim057 <tkim057@ucr.edu>
2015-11-21 11:03:33 +0000

    kyle sucks

--- problems/p12/twos.h
+++ problems/p12/twos.h
@@ -1,7 +1,5 @@
 static int twos(int n){
-	    int n = 250;
         int count=0;
-        int counter = 0;
         for(int i = 1; i <= n; i++){
             if((i % 10)==2){
                count++;
